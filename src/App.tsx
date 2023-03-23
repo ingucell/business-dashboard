0fb5c69dd3d2e8ae6eba195dd54a06f0fb460111
ingucell <mathiaslawson70@gmail.com>
2023-03-23 16:20:52 +0000

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import {ThemeProvider, Box } from "@mui/material"
+import { createTheme } from "@mui/system"
+import { useMemo } from "react"
+import { themeSettings } from './theme'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { CssBaseline } from "@mui/material"
+import Navbar from '@/scenes/navbar'
+
 function App() {
- 
+
+    const theme = useMemo(() => createTheme(themeSettings), [])
+
   return (
     <div className="app">
-      
+      <BrowserRouter>
+      <CssBaseline />
+       <ThemeProvider theme={theme}>  
+              <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+                <Navbar />
+                  <Routes>
+                      <Route path='/' element={<div>dash page</div>}/>
+                      <Route path='/predictions' element={<div>predictions page</div>}/>
+                  </Routes>
+              </Box>
+       </ThemeProvider>
+       </BrowserRouter>
     </div>
   )
 }
