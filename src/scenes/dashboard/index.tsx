b70fab4e21d888b803b874651ced992af05173cf
ingucell <mathiaslawson70@gmail.com>
2023-04-04 17:44:54 +0000

    responsivness

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,8 +1,9 @@
 import {Box, useTheme} from '@mui/material'
+import {useMediaQuery} from '@mui/material'
 
 type Props = {}
 
-const gridTemplate = `
+const gridTemplateLargeScreens = `
  "a b c"
  "a b c"
  "a b c"
@@ -14,15 +15,56 @@ const gridTemplate = `
  "g h j"
  "g h j"
  `
+const gridTemplateSmallScreens = `
+"a"
+"a"
+"a"
+"a"
+"b"
+"b"
+"b"
+"b"
+"c"
+"c"
+"c"
+"d"
+"d"
+"d"
+"e"
+"e"
+"f"
+"f"
+"f"
+"g"
+"g"
+"g"
+"h"
+"h"
+"h"
+"h"
+"i"
+"i"
+"j"
+"j"
+`
 
 const Dashboard = (props: Props) => {
   const { palette } = useTheme();
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
   return (
-    <Box width='100%' height='100%' display='grid' gap='1.5rem' sx={{
+    <Box width='100%' height='100%' display='grid' gap='1.5rem'
+     sx={isAboveMediumScreens ? {
       gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
       gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-      gridTemplateAreas: gridTemplate, 
-    }}>
+      gridTemplateAreas: gridTemplateLargeScreens, 
+    }:{
+      gridTemplateColumns: "1fr",
+      gridTemplateRows: "80px",
+      gridTemplateAreas: gridTemplateSmallScreens,
+    }
+  }
+    
+    >
         <Box bgcolor="#fff" gridArea='a'></Box>
         <Box bgcolor="#fff" gridArea='b'></Box>
         <Box bgcolor="#fff" gridArea='c'></Box>
