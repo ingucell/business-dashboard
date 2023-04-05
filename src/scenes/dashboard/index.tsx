942afddefa19de44268531dcd66c090c3ab6e224
ingucell <mathiaslawson70@gmail.com>
2023-04-05 01:16:17 +0000

    color & shadow

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,5 +1,6 @@
-import {Box, useTheme} from '@mui/material'
+import {Box} from '@mui/material'
 import {useMediaQuery} from '@mui/material'
+import DashboardBox from '@/components/DashboardBox'
 
 type Props = {}
 
@@ -49,7 +50,6 @@ const gridTemplateSmallScreens = `
 `
 
 const Dashboard = (props: Props) => {
-  const { palette } = useTheme();
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
   return (
     <Box width='100%' height='100%' display='grid' gap='1.5rem'
@@ -65,16 +65,17 @@ const Dashboard = (props: Props) => {
   }
     
     >
-        <Box bgcolor="#fff" gridArea='a'></Box>
-        <Box bgcolor="#fff" gridArea='b'></Box>
-        <Box bgcolor="#fff" gridArea='c'></Box>
-        <Box bgcolor="#fff" gridArea='d'></Box>
-        <Box bgcolor="#fff" gridArea='e'></Box>
-        <Box bgcolor="#fff" gridArea='f'></Box>
-        <Box bgcolor="#fff" gridArea='g'></Box>
-        <Box bgcolor="#fff" gridArea='h'></Box>
-        <Box bgcolor="#fff" gridArea='i'></Box>
-        <Box bgcolor="#fff" gridArea='j'></Box>
+      
+        <DashboardBox  gridArea='a'></DashboardBox>
+        <DashboardBox  gridArea='b'></DashboardBox>
+        <DashboardBox  gridArea='c'></DashboardBox>
+        <DashboardBox  gridArea='d'></DashboardBox>
+        <DashboardBox  gridArea='e'></DashboardBox>
+        <DashboardBox  gridArea='f'></DashboardBox>
+        <DashboardBox  gridArea='g'></DashboardBox>
+        <DashboardBox  gridArea='h'></DashboardBox>
+        <DashboardBox  gridArea='i'></DashboardBox>
+        <DashboardBox  gridArea='j'></DashboardBox>
     </Box>
   )
 }
