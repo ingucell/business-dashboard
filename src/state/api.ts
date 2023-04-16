e95f9e29429c63951246e80b7771dce07b7421dd
ingucell <mathiaslawson70@gmail.com>
2023-04-16 22:32:04 +0000

    first chart

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,15 +1,16 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { GetKpisResponse } from './types';
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL}),
     reducerPath: 'main',
     tagTypes: ["Kpis"],
-    endpoints: (build) => ({
-        getKpis: build.query({
+    endpoints: (build) => ({ 
+        getKpis: build.query<Array<GetKpisResponse>, void>({
             query: () => "kpi/kpis/",
             providesTags: ["Kpis"]
         })
     })
 })
 
-export const { useGetKpisQuery } = api
+export const { useGetKpisQuery } = api;
